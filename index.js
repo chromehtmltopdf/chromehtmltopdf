392e057c0e4a3310ae5d510c9a1fc244c6ae8688
Jason Gowan <jgowan@ziprecruiter.com>
2017-05-28 18:17:45 -0700

    add support for local files

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ const path = require('path');
 const pdf_capture = require('./lib/pdf_capture');
 const png_capture = require('./lib/png_capture');
 const jpeg_capture = require('./lib/jpeg_capture');
+const page_driver = require('./lib/page_driver');
 
 const jpeg_extnames = {
     '.jpg': null, '.jpeg': null, '.jpe': null,
     '.jif': null, '.jfif': null, '.jfi': null
 };
 
+function delay(ms) {
+   return new Promise(function(resolve) {
+       setTimeout(resolve, ms)
+   });
+}
 module.exports = function(url, filePath, options) {
     CDP(options, (client) => {
         // Extract used DevTools domains.
         const Network = client.Network;
         const Page = client.Page;
+        const pageDriver = new page_driver.PageDriver(client);
 
         if (typeof(options.capture_strategy) === 'undefined') {
             options.capture_strategy = pdf_capture;
@@ -36,28 +43,32 @@ module.exports = function(url, filePath, options) {
         Promise.all([
             Network.enable(),
             Page.enable()
-        ]).then(() => {
+        ]).then((result) => {
             if (options.userAgent) {
-                Network.setUserAgentOverride({userAgent: options.userAgent}).then(() => {
-                    return Page.navigate({url: url}).catch((err) => {
-                        console.error('Unable to navigate to %s :', url, err)
-                        client.close();
-                    });
-                }).catch((err) => {
-
-                    console.error('Unable to set user agent %s :', options.userAgent, err)
-                    client.close();
-                });
+                result = pageDriver.setUserAgent(options.userAgent);
+            }
+            return result;
+        }).then(() => {
+            if (url.startsWith('http:') || url.startsWith('file:')) {
+                return pageDriver.navigate(url);
             } else {
-                return Page.navigate({url: url}).catch((err) => {
-                    console.error('Unable to navigate to %s :', url, err)
-                    client.close();
+                fs.readFile(url, {encoding: 'utf-8'}, (err, html) => {
+                    if (err) {
+                        client.close();
+                        throw err
+                    }
+                    return pageDriver.setContent(html);
                 });
             }
-            });
+        }).catch((err) => {
+            console.error(err);
+            console.error(err.stack);
+            client.close();
+            process.exit(1);
+        });
 
         Page.loadEventFired(() => {
-            setTimeout(() => {
+            delay(options.javascriptDelay || 0).then(() => {
                 options.capture_strategy(Page, options).then((result) => {
                     if (filePath) {
                         const data = Buffer.from(result.data, 'base64')
@@ -70,7 +81,7 @@ module.exports = function(url, filePath, options) {
                     console.error('Screenshot Failed: ', err);
                     client.close();
                 });
-            }, options.javascriptDelay || 0);
+            });
         });
     }).on('error', (err) => {
         console.error('Cannot connect to browser:', err);
